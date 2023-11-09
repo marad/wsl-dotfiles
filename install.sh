055f054eb41c9741b5e8445bac305c1b6e655906
Marcin Radoszewski <moriturius@gmail.com>
2023-11-09 16:17:04 +0100

    Add tmux and gitconfig

--- install.sh
+++ install.sh
@@ -18,3 +18,8 @@ ln -s "$SCRIPT_DIR/nvim" ~/.config/nvim
 rm -rf ~/.config/starship.toml
 ln -s "$SCRIPT_DIR/starship.toml" ~/.config/starship.toml
 
+rm -rf ~/.gitconfig
+ln -s "$SCRIPT_DIR/gitconfig" ~/.gitconfig
+
+rm -rf ~/.tmux.conf
+ln -s "$SCRIPT_DIR/tmux.conf" ~/.tmux.conf
