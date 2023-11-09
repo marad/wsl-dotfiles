874c8c52009339c09387b7f663a74579310a1f03
Marcin Radoszewski <moriturius@gmail.com>
2023-11-09 14:29:35 +0100

    Use ansible for reproducible installation

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
-sudo pacman --noconfirm --needed -Syu neovim fzf rustup eza ripgrep bat starship
+sudo pacman --noconfirm -Syu ansible
+
+ansible-playbook ansible/wsl.yaml
 
 BASHRC="$SCRIPT_DIR/bashrc"
 
@@ -13,10 +15,3 @@ ln -s $BASHRC ~/.bashrc
 rm -rf ~/.config/nvim
 ln -s "$SCRIPT_DIR/nvim" ~/.config/nvim
 
-if ! type "cargo" > /dev/null; then
-	rustup install stable
-fi
-
-if ! type "jumper" > /dev/null; then
-	cargo install jumper
-fi
